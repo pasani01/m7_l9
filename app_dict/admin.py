5044434c84c70c0874c445dd0f16d6bee096d9a2
pasani01 <nasirdin520@gmail.com>
2025-05-31 16:51:28 +0500

    home work

--- app_dict/admin.py
+++ app_dict/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Catagory
 
 # Register your models here.
+@admin.register(Catagory)
+class CatagoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'catagory_name')
--- app_dict/views.py
+++ app_dict/views.py
@@ -1,11 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 
 from .models import Dictionary
 from .serializers import DictListSerializer
-
+from .pagenations import DictPagination
 
 class DictViewSet(ModelViewSet):
-    queryset=Dictionary.objects.all()
-    lookup_field='id'
+    queryset = Dictionary.objects.all()
+    lookup_field = 'id'
+    serializer_class = DictListSerializer
+
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['word_ru', 'word_uz_k', 'word_uz_l', 'word_en', 'word_tr', 'catagory__catagory_name']
+    ordering_fields = ['field1', 'field2']  
+
+    pagination_class = DictPagination
+
     def get_serializer_class(self):
         return DictListSerializer
